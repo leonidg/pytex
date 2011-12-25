e4bb74041fe2acb02be970a2cfe5abb5ef8e8ce2
Leonid Grinberg <leonidg@mit.edu>
2011-12-25 00:19:19 -0500

    Removed TeXEmptyCommand from __init__.py

--- pytex/__init__.py
+++ pytex/__init__.py
@@ -1,4 +1,4 @@
-from base.objects import TeXObject, TeXCommand, TeXEmptyCommand
+from base.objects import TeXObject, TeXCommand
 from base.collections import TeXCollection, TeXSet, TeXGroup, TeXInlineMath, TeXBlockMath, TeXRow
 import latex
 import util
