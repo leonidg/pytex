c801b1c008ffcaa554d2239cd5e7df3277ff240a
Leonid Grinberg <leonidg@mit.edu>
2012-02-25 20:22:07 -0500

    Added an operator math function and superscript() and subscript() wrappers

--- pytex/util.py
+++ pytex/util.py
@@ -38,3 +38,27 @@ def tex_escape_string(s):
         else:
             escaped.append(c)
     return "".join(escaped)
+
+def unmath(x):
+    return TeXCommand("mbox", x)
+
+def binary_math_operator(x, y, operator, math=(True, True)):
+    """
+    A generic function that combines two arguments with a binaryn
+    operator. The optional "math" argument is a tuple specifying
+    whether either the first or second argument should be treated as
+    mathematical input.
+    """
+    # We'll use the identity function if we want math input (since
+    # we'll be in ensure_math()) and otherwise unmath().
+    identity = lambda x: x
+    math_wrapper = [identity if math[i] else unmath  for i in (0, 1)]
+    return ensure_math(TeXSet([math_wrapper[0](TeXObject(x)),
+                               TeXObject(operator, raw=True),
+                               math_wrapper[1](TeXObject(y))]))
+
+def superscript(x, y, math=(True, True)):
+    return binary_math_operator(x, y, operator="^", math=math)
+
+def subscript(x, y, math=(True, True)):
+    return binary_math_operator(x, y, operator="_", math=math)
